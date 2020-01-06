abe55f9ce84275100587bdbe13af954f24a060d4
JEONG-MW <51899261+Jeong-Mw@users.noreply.github.com>
2020-01-06 19:58:14 +0900

    .

--- src/router/UserInfo.js
+++ src/router/UserInfo.js
@@ -18,11 +18,26 @@ export default class UserInfo extends React.Component {
                         <div className="box-pie box">
                             <span className="subtitle">win rating</span>
                             <div className="piechart"><PieChart
-                                animate={true}
+                                animate={false}
                                 data={[
-                                    { title: 'One', value: 10, color: '#E38627' },
-                                    { title: 'Two', value: 15, color: '#C13C37' },
+                                    {
+                                        color: '#843B94',
+                                        title: 'two',
+                                        value: 20
+                                    },
+                                    {
+                                        color: '#9D74A6',
+                                        title: 'One',
+                                        value: 1000
+                                    }
                                 ]}
+                                label={function noRefCheck() { }}
+                                labelPosition={50}
+                                labelStyle={{
+                                    fill: '#121212',
+                                    fontFamily: 'sans-serif',
+                                    fontSize: '5px'
+                                }}
                             /></div>
                             <span className="data">(%)</span>
                         </div>
@@ -39,38 +54,39 @@ export default class UserInfo extends React.Component {
                     </div>
 
                     <div className="game">
-                        <div className="game-box">
-                            <span className="date">(date)</span>
-                            <span className="time">(time)</span>
+                        <div className="game-b game-box">
+                            <div className="datatext mode">(meisterboi)</div>
+                            <div className="datatext bar"></div>
+                            <div className="datatext date">(date)</div>
+                            <div className="datatext time">(time)</div>
                         </div>
-                    </div>
+                        {/* <div className="game-stats"> */}
+                        <div className="game-b">
+                            <div className="datatext datatitletext kill">kill</div>
+                            <div className="datatext damage">(kill)</div>
+                        </div>
+                        <div className="game-b">
+                            <div className="datatext datatitletext kill">damage</div>
+                            <div className="datatext damage">(damage)</div>
+                        </div>
+                        <div className="game-b">
+                            <div className="datatext datatitletext kill">damage</div>
+                            <div className="datatext damage">(damage)</div>
+                        </div>
+                        <div className="game-b">
+                            <div className="datatext datatitletext kill">damage</div>
+                            <div className="datatext damage">(damage)</div>
+                        </div>
+                        <div className="game-b">
+                            <div className="datatext datatitletext kill">damage</div>
+                            <div className="datatext damage">(damage)</div>
+                        </div>
+                        <div className="game-b">
+                            <div className="datatext datatitletext kill">damage</div>
+                            <div className="datatext damage">(damage)</div>
+                        </div>
 
-                    <div className="game">
-                        <div className="game-box">
-                            <span className="date">(date)</span>
-                            <span className="time">(time)</span>
-                        </div>
-                    </div>
-
-                    <div className="game">
-                        <div className="game-box">
-                            <span className="date">(date)</span>
-                            <span className="time">(time)</span>
-                        </div>
-                    </div>
-
-                    <div className="game">
-                        <div className="game-box">
-                            <span className="date">(date)</span>
-                            <span className="time">(time)</span>
-                        </div>
-                    </div>
-
-                    <div className="game">
-                        <div className="game-box">
-                            <span className="date">(date)</span>
-                            <span className="time">(time)</span>
-                        </div>
+                        {/* </div> */}
                     </div>
                 </div>
             </div>
