7a8520b258eadf5109f724b869a44412d2af05c4
JEONG-MW <51899261+Jeong-Mw@users.noreply.github.com>
2020-01-06 16:10:29 +0900

    .
    layout 수정

--- src/router/UserInfo.js
+++ src/router/UserInfo.js
@@ -13,63 +13,65 @@ export default class UserInfo extends React.Component {
                 <div className="bg-box">
 
                     <h1 className="username">USERNAME</h1>
+                    <div className="userinfobox">
 
-                    <div className="box">
-                        <h2>rating</h2>
-                        <div className="piechart"><PieChart
-                            radius={20}
-                            animate={true}
-                            data={[
-                                { title: 'One', value: 10, color: '#E38627' },
-                                { title: 'Two', value: 15, color: '#C13C37' },
-                            ]}
-                        /></div>
-                        <div className="data">(%)</div>
-                    </div>
+                        <div className="box-pie box">
+                            <span className="subtitle">win rating</span>
+                            <div className="piechart"><PieChart
+                                animate={true}
+                                data={[
+                                    { title: 'One', value: 10, color: '#E38627' },
+                                    { title: 'Two', value: 15, color: '#C13C37' },
+                                ]}
+                            /></div>
+                            <span className="data">(%)</span>
+                        </div>
 
-                    <div className="box">
-                        <h2>play time</h2>
-                        <div className="data">100%</div>
-                    </div>
-                    <div className="box">
-                        <h2>kill</h2>
-                        <div className="data">100%</div>
+                        <div className="box">
+                            <span className="subtitle">playtime</span>
+                            <span className="data">1010</span>
+                        </div>
+                        <div className="box">
+                            <span className="subtitle">kill</span>
+                            <span className="data">1010</span>
+                        </div>
+
                     </div>
 
                     <div className="game">
                         <div className="game-box">
-                            <div className="date">(date)</div>
-                            <div className="time">(time)</div>
+                            <span className="date">(date)</span>
+                            <span className="time">(time)</span>
                         </div>
                     </div>
 
                     <div className="game">
                         <div className="game-box">
-                            <div className="date">(date)</div>
-                            <div className="time">(time)</div>
+                            <span className="date">(date)</span>
+                            <span className="time">(time)</span>
                         </div>
                     </div>
 
                     <div className="game">
                         <div className="game-box">
-                            <div className="date">(date)</div>
-                            <div className="time">(time)</div>
+                            <span className="date">(date)</span>
+                            <span className="time">(time)</span>
                         </div>
                     </div>
 
                     <div className="game">
                         <div className="game-box">
-                            <div className="date">(date)</div>
-                            <div className="time">(time)</div>
+                            <span className="date">(date)</span>
+                            <span className="time">(time)</span>
                         </div>
                     </div>
 
                     <div className="game">
                         <div className="game-box">
-                            <div className="date">(date)</div>
-                            <div className="time">(time)</div>
+                            <span className="date">(date)</span>
+                            <span className="time">(time)</span>
                         </div>
-                    </div>                    
+                    </div>
                 </div>
             </div>
         );
